d95bf9100942f064b5d88f4674483f2959f1edaf
Magnus Karlsson <Magnus.Karlsson@diadrom.se>
2022-11-30 23:20:09 +0100

    Added options to input.rs

--- src/input.rs
+++ src/input.rs
@@ -3,6 +3,21 @@ use std::fs::File;
 use std::io::{BufReader, BufRead};
 use anyhow::{Result,Ok};
 
+pub trait InputParser {
+    fn parse_line(&mut self, line:&String) -> Result<()>;
+}
+
+pub fn parse_input_file<P>(file_name: &str, parser: &mut P) -> Result<()> where
+    P: InputParser{
+    let file = File::open(file_name).unwrap();
+    let reader = BufReader::new(file);
+
+    for line in reader.lines() {
+        parser.parse_line(&(line.unwrap()))?;
+    };
+    Ok(())
+}
+
 pub fn get_vector_from_file<T, F>(file_name: &str, mut transform: F) -> Vec<T> where
     F: FnMut(&str) -> T {
     let mut all_of_them: Vec<T> = Vec::new();
@@ -15,6 +30,8 @@ pub fn get_vector_from_file<T, F>(file_name: &str, mut transform: F) -> Vec<T> w
     }
     return all_of_them;
 }
+
+
 
 pub fn get_private_session() -> Result<String>{
     let file = File::open("my_secret.txt")?;
